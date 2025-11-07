e614c977f87eb79b8c67312d404719739f7391f4
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 19:04:45 +0700

    Alur navigasi aplikasi

--- app/src/main/java/com/example/questnavigasiui_050/Navigasi.kt
+++ app/src/main/java/com/example/questnavigasiui_050/Navigasi.kt
@@ -1,11 +1,16 @@
 package com.example.questnavigasiui_050
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigasiui_050.view.FormIsian
+import com.example.questnavigasiui_050.view.TampilData
 
 enum class Navigasi{
     Formulirku,
@@ -22,8 +27,28 @@ fun DataApp(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
 
-
-        )
+            modifier = Modifier.padding(paddingValues = isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    //pilihanJK = jenisK.map { id -> konteks.resources.getString(id) },
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+            composable(route = Navigasi.Detail.name){
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
     }
+}
 
+private fun cancelAndBackToFormulir(
+    navController: NavController
+){
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
 }
