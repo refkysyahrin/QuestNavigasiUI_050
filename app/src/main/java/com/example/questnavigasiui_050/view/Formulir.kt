4bc3e10b6ec9755b5a61b44a7e2f2f973155486b
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 15:07:51 +0700

    Membuat Scaffold untuk topBar

--- app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
@@ -1,7 +1,16 @@
 package com.example.questnavigasiui_050.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_050.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +19,14 @@ fun FormIsian(
     OnSubmitBtnClick : () -> Unit
 
 ){
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.black))
+                )}
+            ){ isi
+        })
 
 }
