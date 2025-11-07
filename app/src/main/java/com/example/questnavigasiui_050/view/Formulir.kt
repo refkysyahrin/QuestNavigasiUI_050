f377441243450df905b2393455d6b65774e320a6
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 14:59:22 +0700

    Membuat fun FormIsian

--- app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
@@ -1,2 +1,10 @@
 package com.example.questnavigasiui_050.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+
+)
