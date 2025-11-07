198e61921ab97b85d7474a121c248e11e375eded
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 15:12:30 +0700

    Membuat form nama lengkap

--- app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
@@ -3,7 +3,9 @@ package com.example.questnavigasiui_050.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.questnavigasiui_050.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,9 +37,18 @@ fun FormIsian(
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(width = 250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
 
         }
 
-        })
+        }
 
 }
