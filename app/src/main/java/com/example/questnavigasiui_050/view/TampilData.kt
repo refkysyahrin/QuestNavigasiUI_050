a4bce3e473cd0c65068e1e017a8cd578cc069e1a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 19:02:23 +0700

    Mengganti warna TopAppBar, divider, dan mengganti tipe font

--- app/src/main/java/com/example/questnavigasiui_050/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/TampilData.kt
@@ -38,8 +38,8 @@ fun TampilData(
     Scaffold (modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White)},
-                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.black))
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.Black)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.birumuda))
             )
         }){  isiRuang->
         Column(modifier = Modifier.padding(paddingValues = isiRuang),
@@ -48,11 +48,11 @@ fun TampilData(
                 items.forEach { item ->
                     Column {
                         Text(text = item.first.uppercase(), fontSize = 16.sp)
-                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.SansSerif, fontSize = 22.sp)
                     }
-                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                    HorizontalDivider(thickness = 1.dp, color = Color.Black)
                 }
-                Spacer(modifier = Modifier.height(height = 10.dp))
+                Spacer(modifier = Modifier.height(height = 30.dp))
                 Button(
                     modifier = Modifier.fillMaxWidth(),
                     onClick = onBackBtnClick) {
