822416b1ae25f721fd4f23a8cd96342274b4a8ca
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 18:17:33 +0700

    Membuat fun DataApp dan controller

---
+++ app/src/main/java/com/example/questnavigasiui_050/Navigasi.kt
@@ -0,0 +1,19 @@
+package com.example.questnavigasiui_050
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi{
+    Formulirku,
+
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+
+}
