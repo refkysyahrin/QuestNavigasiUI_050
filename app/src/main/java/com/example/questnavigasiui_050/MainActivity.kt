460757204336c678509c7a7158e15416d4fe5d8e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 19:06:27 +0700

    Mengatur DataApp sebagai content view utama di MainActivity

--- app/src/main/java/com/example/questnavigasiui_050/MainActivity.kt
+++ app/src/main/java/com/example/questnavigasiui_050/MainActivity.kt
@@ -6,12 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questnavigasiui_050.ui.theme.QuestNavigasiUI_050Theme
+import com.example.questnavigasiui_050.view.FormIsian
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuestNavigasiUI_050Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    DataApp()
                 }
             }
         }
