efb3a94b386f4e7f66179aeeb48e57dece82c8db
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 16:03:46 +0700

    Membuat isi val

--- app/src/main/java/com/example/questnavigasiui_050/view/TampilData.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/TampilData.kt
@@ -2,9 +2,17 @@ package com.example.questnavigasiui_050.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.questnavigasiui_050.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()-> Unit
-)
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Refky Muhammad Syahrin"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Laki-laki"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Tasikmalaya"),
+    )
+}
