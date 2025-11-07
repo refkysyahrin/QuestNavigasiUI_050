e5fb30f17929c3e6921d40ea6693f0685a86e32c
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-11-07 15:01:46 +0700

    Membuat fun FormIsian dan mengisi jenisK dengan listOF laki laki dan perempuan

--- app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigasiui_050/view/Formulir.kt
@@ -6,5 +6,9 @@ import androidx.compose.runtime.Composable
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
 
-)
+){
+
+}
